e2ce4c3107d6177ccc2802a52499314d6ed1025a
Prajakta Mhatre <76698736+pramha@users.noreply.github.com>
2021-01-01 14:25:27 +0530

    Update HandleWindowPopup.py

--- HandleWindowPopup.py
+++ HandleWindowPopup.py
@@ -1,1 +1,17 @@
+import pyautogui
+imort logging
+import time
+
+def popup_username():
+  pyautogui.click()
+  pyautogui.typwrite('username', interval=0.20)
+  time.sleep(3)
+  logging.info('successfully entered the username')
+  pyautogui.press('tab')
+  
+def popup_password():
+ pyautogui.typwrite('password', interval=0.20) 
+ time.sleep(3)
+ logging.info('successfully entered the password')
+ pyautogui.press('tab')
 
